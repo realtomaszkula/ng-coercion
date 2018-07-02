ea89eb34d5cc4eab09e9de6fa887e9fdad36d720
Tomasz Kula <tomaszkulawebdev@gmail.com>
2018-07-02 13:45:29 +0200

    refactor: replace mat-toolbar with responsive header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <mat-toolbar color="primary">
+  <header class="mat-elevation-z4">
     <a mat-button routerLink="/coerce-boolean">coerceBooleanProperty</a>
     <a mat-button routerLink="/coerce-number">coerceNumberProperty</a>
     <a mat-button routerLink="/coerce-array">coerceArray</a>
     <a mat-button routerLink="/coerce-css-pixel">coerceCssPixelValue</a>
-  </mat-toolbar>
+  </header>
 
   <main>
     <router-outlet></router-outlet>
@@ -19,6 +19,20 @@ import { Component } from '@angular/core';
     `
       main {
         padding: 1em;
+      }
+
+      header {
+        padding: 12px;
+        color: white;
+        background-color: #3f51b5;
+        display: flex;
+        flex-direction: column;
+      }
+
+      @media (min-width: 400px) {
+        header {
+          flex-direction: row;
+        }
       }
     `
   ]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { MatToolbarModule, MatButtonModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -29,7 +29,6 @@ import { BorderedBoxExamplesComponent } from './coerceCssPixelValue/bordered-box
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
     MatButtonModule,
     RouterModule.forRoot([
       { path: '', redirectTo: 'coerce-boolean', pathMatch: 'full' },
